aa5d2d75add09903ab7898b7e2fae34a54a85394
Moacyr P Neto <Moapneto95@gmail.com>
2024-09-18 14:01:31 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    $('#telefone').mask('(00) 00000-0000');
+
+    $('form').validate({
+        rules:{
+            nome:{
+                required:true
+            },
+            email:{
+                required:true,
+                email:true
+                },
+            telefone:{
+                required:true
+            },
+            aceitoTermos:{
+                required:true
+            }
+        },
+
+        messages:{
+            nome:'Por favor, insira o seu nome.',
+            email:'Por favor, insira seu e-mail.',
+            telefone:'Por favor, insira seu telefone',
+            aceitoTermos: 'Você deve aceitar os termos para continuar.'
+        },
+
+        submitHandler: function(form) {
+            alert("Sua requisição foi enviada para análise. Cadastro completo.");
+            $(form).trigger('reset');
+        },
+
+        invalidHandler:function(evento, validador) {
+            let camposIncompletos = validador.numberOfInvalids();
+            if(camposIncompletos){
+                alert(`Existem ${camposIncompletos} campos incorretos.`)
+            }
+        }
+    })
+});
